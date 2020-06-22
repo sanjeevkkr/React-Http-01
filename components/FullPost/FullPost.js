5277f5a1f52a77a03869eb3c09211f237ae5e548
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-06-22 18:10:26 +0530

    Setting a Default Global Configuration for Axios

--- components/FullPost/FullPost.js
+++ components/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
             // Below if condition is added to break the infinite loop 
             // because componentDidUpdate will call each time when anything will be render.
             if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                 .then(response =>{
                     this.setState({loadedPost: response.data});
                 } )
@@ -22,7 +22,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () =>{
-        axios.delete('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+        axios.delete('/posts/'+this.props.id)
             .then(response =>{
                 console.log(response);
             })
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+//Below axios global setting is not needed here its just for example
+axios.defaults.headers.common['Authorization'] = 'Auth Token';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
     console.log(request);
     return request;
